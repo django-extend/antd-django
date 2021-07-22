f9d29271d643878a297e6e1bb52ccb4daec6fcfe
nick <you@example.com>
2021-07-22 18:08:07 +0800

    首页也进入字典

--- src/utils/components.js
+++ src/utils/components.js
@@ -3,6 +3,7 @@ import { RouteView } from '@/layouts'
 const components = {
   defaults: {
     'menu': RouteView,
+    'dashboard': () => import('@/views/Dashboard'),
     'django': () => import('@/components/Django')
   },
   custom: {
